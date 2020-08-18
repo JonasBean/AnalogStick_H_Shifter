113bb89494ce32b9c858f30ad34f7156d7d0dedb
JonasBean <Jonneybien@web.de>
2020-08-18 13:02:41 +0200

    Reverted Mouse and Hardware Input - it's obviously needed.

--- AnalogStick_H_Shifter/H_Shifter.cs
+++ AnalogStick_H_Shifter/H_Shifter.cs
@@ -713,7 +713,50 @@ namespace AnalogStick_H_Shifter
         internal struct InputUnion
         {
             [FieldOffset(0)]
+            internal MOUSEINPUT mi;
+            [FieldOffset(0)]
             internal KEYBDINPUT ki;
+            [FieldOffset(0)]
+            internal HARDWAREINPUT hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct MOUSEINPUT
+        {
+            internal int dx;
+            internal int dy;
+            internal MouseEventDataXButtons mouseData;
+            internal MOUSEEVENTF dwFlags;
+            internal uint time;
+            internal UIntPtr dwExtraInfo;
+        }
+
+        [Flags]
+        internal enum MouseEventDataXButtons : uint
+        {
+            Nothing = 0x00000000,
+            XBUTTON1 = 0x00000001,
+            XBUTTON2 = 0x00000002
+        }
+
+        [Flags]
+        internal enum MOUSEEVENTF : uint
+        {
+            ABSOLUTE = 0x8000,
+            HWHEEL = 0x01000,
+            MOVE = 0x0001,
+            MOVE_NOCOALESCE = 0x2000,
+            LEFTDOWN = 0x0002,
+            LEFTUP = 0x0004,
+            RIGHTDOWN = 0x0008,
+            RIGHTUP = 0x0010,
+            MIDDLEDOWN = 0x0020,
+            MIDDLEUP = 0x0040,
+            VIRTUALDESK = 0x4000,
+            WHEEL = 0x0800,
+            XDOWN = 0x0080,
+            XUP = 0x0100
+
         }
 
         [StructLayout(LayoutKind.Sequential)]
@@ -725,6 +768,16 @@ namespace AnalogStick_H_Shifter
             internal int time;
             internal UIntPtr dwExtraInfo;
         }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct HARDWAREINPUT
+        {
+            internal int uMsg;
+            internal short wParamL;
+            internal short wParamH;
+        }
+
+
 
         [Flags]
         internal enum KEYEVENTF : uint
